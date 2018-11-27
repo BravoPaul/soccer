cf1c93f2fe0418c4666ef6b92edfdc481ebd7dcc
坤月 <pankun.pk@alibaba-inc.com>
2018-11-27 12:33:54 +0800

    只有主客队是错的

--- src/train.py
+++ src/train.py
@@ -11,7 +11,7 @@ from sklearn.decomposition import PCA
 
 
 
-def train(train_data,label):
+def train(train_data,label,test_data,ground_truth):
 
     match = pd.read_csv('/Users/kunyue/project_personal/soccer/data/英超/soccer/game_end.csv')
 
@@ -36,21 +36,43 @@ def train(train_data,label):
         pred = xgboost_model.predict(train_data[test_index])
         # 标准答案
         ground_truth = label[test_index]
+
+    # xgboost_model = xgb.XGBClassifier().fit(train_data, label)
+    # pred = xgboost_model.predict(test_data)
+    #
     print("混淆矩阵:")
     print(confusion_matrix(ground_truth, pred))
     print("准确率：")
     print(metrics.accuracy_score(ground_truth, pred))
-6
 
 
 
 if __name__ == "__main__":
-    pd_train_data = pd.read_csv('/Users/kunyue/project_personal/soccer/data/train_data/train_data.csv')
+
+    data = pd.read_csv('/Users/kunyue/project_personal/soccer/data/train_data/train_data.csv')
+    pd_train_data = data.sample(frac=0.8)
+    pd_train_data_test = data[~data.index.isin(pd_train_data.index)]
+    pd_train_data = pd.concat([pd_train_data,pd_train_data,pd_train_data,pd_train_data],axis=0)
+
     pd_label_data = pd_train_data['win_lose_label']
+    pd_label_data_test = pd_train_data_test['win_lose_label']
+
+    del pd_train_data['Unnamed: 0']
+    del pd_train_data['Unnamed: 0.1']
     del pd_train_data['home_team']
     del pd_train_data['away_team']
     del pd_train_data['date']
     del pd_train_data['win_lose_label']
+
+    del pd_train_data_test['Unnamed: 0']
+    del pd_train_data_test['Unnamed: 0.1']
+    del pd_train_data_test['home_team']
+    del pd_train_data_test['away_team']
+    del pd_train_data_test['date']
+    del pd_train_data_test['win_lose_label']
+
+    print(pd_train_data.columns)
     pd_train_data = pd_train_data.fillna(0)
-    train(pd_train_data.values,pd_label_data.values)
+    pd_train_data_test = pd_train_data_test.fillna(0)
+    train(pd_train_data.values,pd_label_data.values,pd_train_data_test.values,pd_label_data_test.values)
 
